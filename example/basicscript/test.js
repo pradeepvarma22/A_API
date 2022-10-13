a1cf2b10be2f812ba3f4f4a2d30c24f3ad286f0a
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-13 21:53:18 +0530

    example: how users can query
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

---
+++ example/basicscript/test.js
@@ -0,0 +1,35 @@
+import fetch from 'cross-fetch';
+
+const API_KEY_PROD = "https://airclone-gules.vercel.app/api/graphql/validate/rnsstsatrHatth.aniaattiit";
+const API_KEY_QA = "http://localhost:3000//api/graphql/validate/rnsstsatrHatth.aniaattiit";
+
+const myQuery = `
+{
+    userAccounts(first: 5) {
+        id
+        address
+    }  
+}
+`
+
+async function main() {
+    const res = await fetch(API_KEY_QA, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            graphName: "USDT",
+            myQuery: myQuery
+        })
+    })
+
+    const data = await res.json()
+    console.log(data)
+}
+
+async function _main() {
+    await main()
+}
+
+_main()
